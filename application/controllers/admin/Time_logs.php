9d0329fcc5cfe1440108095a2f52dc57ce60da2a
SabeerAhmedS <sabeerahmed.s@limitscale.io>
2025-04-30 11:55:04 +0530

    new changed

--- application/controllers/admin/Time_logs.php
+++ application/controllers/admin/Time_logs.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Time_logs extends CI_Controller { // Change Your_controller to your actual controller name
+class Time_logs extends CI_Controller { 
 
     public function __construct() {
         parent::__construct();
