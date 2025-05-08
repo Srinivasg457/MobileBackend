1c904806b3f5205c22ca8316388372610d228846
Venkatesh-52168 <venkyarruri@gmail.com>
2025-05-08 09:43:10 +0530

    remove the versitical scroll bar

--- application/views/admin/time_cards.php
+++ application/views/admin/time_cards.php
@@ -2,6 +2,11 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <style>
+        .content-wrapper{
+    height: unset !important;
+    min-height: unset !important;
+}
+
         #activityChart {
             max-width: 100%;
             height: 400px;
